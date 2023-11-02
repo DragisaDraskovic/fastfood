ee7ad28f62cc1f72b9b201200cb644f37f182e94
DragisaDraskovic <dragisadraskovic2@gmail.com>
2023-11-02 01:10:57 +0100

    Add wellcome message

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,10 +1,16 @@
 import React from 'react'
+
+import './MainPage.css'
+
 import Header from '../header/Header'
+import FirstCarousel from '../carousel/firstCarousel/FirstCarousel'
 
 const MainPage = () => {
   return (
     <>
         <Header />
+        <h2 className='wellcomeText'>Dobro došli!</h2>
+        <FirstCarousel />
     </>
   )
 }
