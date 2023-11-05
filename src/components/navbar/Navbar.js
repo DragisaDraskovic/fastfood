d6d6f7bf29e1332016137426caf2f9910f37f6d3
DragisaDraskovic <dragisadraskovic2@gmail.com>
2023-11-05 20:46:13 +0100

    improved menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,27 +2,27 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import { GiPalmTree as Palma } from 'react-icons/gi'
 import { VscChromeClose as X } from 'react-icons/vsc'
+import { animated, useSpring } from 'react-spring'
 
 import burger from '../../assets/image/burger1.jpg'
 
 const Navbar = () => {
 
 
-  const [ isMenuOpen, setIsMenuOpen ] = useState(false)
+  const [ isToogledMenu, setIsToogledMenuOpen ] = useState(false)
 
-  const opetMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-
+  const toogleMenuHandler = () => {
+    setIsToogledMenuOpen(!isToogledMenu)
   }
 
-  const closeMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
+  const { x } = useSpring({
+    x: isToogledMenu ? 0 : 100
+  })
 
   return (
     <>
     <div className='header'>
-        <div className='menu' onClick={opetMenu}>
+        <div className='menu' onClick={toogleMenuHandler}>
           <div className='burgerClass'></div>
           <div className='burgerClass'></div>
           <div className='burgerClass'></div>
@@ -30,10 +30,11 @@ const Navbar = () => {
         <div className='navbarTitle'>Flamingo</div>
         <div><Palma size='40px' color='#F7E987'/></div>
     </div>
-    { isMenuOpen &&
-      <div className='menuClass'>
+    { isToogledMenu &&
+      <animated.div className='menuClass'
+        style={{ transform: x.interpolate(x => `translate3d(${x * -1}%, 0, 0)`) }}>
         <div className='menuXClass'>
-          <X size='30px' color='#9AA0A4' onClick={closeMenu}/>
+          <X size='30px' color='#9AA0A4' onClick={toogleMenuHandler}/>
         </div>
         <div className='menuOptions'>
           <img src={burger} className='imgBurgerMenu'/>
@@ -42,7 +43,7 @@ const Navbar = () => {
           <p className='menuText'>O NAMA</p>
           <p className='menuText'>KONTAKT</p>
         </div>
-      </div>
+      </animated.div>
     }
     </>
   )
