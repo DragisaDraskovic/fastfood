5614c6a5f6594fdffa69edd0a9cec0f5b714eb9b
DragisaDraskovic <dragisadraskovic2@gmail.com>
2023-11-23 03:32:35 +0100

    delivery finish

--- src/components/delivery/Delivery.js
+++ src/components/delivery/Delivery.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Delivery.css'
 
+import deliveryImg from '../../assets/image/Delivery - Copy.jpg'
+
+
 const Delivery = () => {
+
+  const [ flag, setFlag ] = useState(false)
+
+  useEffect(() => {
+    for(var second = 0; second < 10; second++) {
+       setFlag(true)
+    }
+  },[])
+
+
   return (
-    <div className='imena'>Delivery</div>
+    <div className='deliveryContainer'>
+      <p className='deliveryDescriptionText'>DOSTAVU VRŠIMO U SARADNJI SA NAŠIM PARTNEROM "DOSTAVA GOCA"</p>
+      <p className='deliveryDescriptionNumber'> 0631122333</p>
+      {flag ? <img className='deliveryImgFirst' src={deliveryImg} /> : <img className='deliveryImgSecond' src={deliveryImg} /> }
+    </div>
   )
 }
 
